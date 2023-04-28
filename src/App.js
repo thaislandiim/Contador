18d2797e2614374c009428e75100834f0aec6390
Thais Landim <thaislandimc@outlook.com>
2023-04-28 14:39:49 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 
-
 import {useState, useEffect} from 'react'
 
 function App() {
   const [contador, setContador] = useState(0);
   const [contadorAntigo, setContadorAntigo] = useState(0);
-  const [operacaoParImpar, setOperacaoParImpar] = useState("")
-  const [operacaoMult10, setOperacaoMult10] = useState("")
+  const [operacaoMult10, setOperacaoMult10] = useState(false);
+  const [operadorPar, setOperadorPar] = useState(false);
 
   const handleContadorMais = () => {
     setContadorAntigo(contador)
@@ -19,17 +18,8 @@ function App() {
   }
 
   useEffect(() => {
-    if (contador % 2 == 0) {
-      setOperacaoParImpar("Esse número é par")
-    } else {
-      setOperacaoParImpar("Esse número é impar")
-    }
-
-    if(contador % 10 == 0){
-      setOperacaoMult10("Esse número é múltiplo de 10")
-    } else {
-      setOperacaoMult10(" ")
-    }
+    setOperadorPar(contador % 2 == 0);
+    setOperacaoMult10(contador % 10 == 0);
   }, [contador])
 
   return (
@@ -43,29 +33,24 @@ function App() {
             <h2>Antigo: {contadorAntigo}</h2>
           </div>
           <div class="col">
-            <h2>Atual: {contador}</h2> 
-          </div>
-        </div>
-        <div class="row mt-2">
-          <div class="col">
-            <p>Par ou impar:</p>
-          </div>
-          <div class="col">
-            <p>{operacaoParImpar}</p>
-          </div>
-        </div>
-        <div class="row mt-2">
-          <div class="col">
-            <p>Múltiplo de 10:</p>
-          </div>
-          <div class="col">
-            <p>{operacaoMult10}</p>
+            <h2>Atual:
+              <span className={operadorPar ? "text-success" : "text-danger"}> {contador}
+              </span>
+            </h2> 
           </div>
         </div>
       </div>
-
+      <div class="card text-center mb-3">
+        {operadorPar ? "Esse número é par" : "Esse número é impar"}
+      </div>
+        {operacaoMult10 ? 
+        <div class="card text-center mb-3">Esse número é múltiplo de 10</div>
+         : null
+        }
+      <div class="text-center">
       <button class="btn btn-primary px-3" onClick={handleContadorMenos}> - </button>
       <button class="btn btn-primary px-3 ms-3" onClick={handleContadorMais}> + </button>
+      </div>
     </div>
   );
 }
